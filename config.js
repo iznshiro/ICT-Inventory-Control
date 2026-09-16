997885e67c2f9b3c92b7a52fe747cb29d36e4de7
iznshiro <iznshiro@gmail.com>
2026-09-16 15:16:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * KONFIGURASI FRONTEND
+ * ---------------------
+ * Isi APPS_SCRIPT_URL dengan URL Web App hasil deploy Google Apps Script
+ * (Code.gs). Lihat README.md bagian "Setup" untuk cara mendapatkannya.
+ *
+ * Contoh:
+ * const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycb.../exec';
+ */
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw0cfbfGY9gU0yArG-OEjnQfdqXc3qC7IszQY-ONpGJvL6bsHAbK4uC71aC0Tx3QQgj/exec';
